33596f09d7b9e0bfcafc242aed3dc681a69a4228
M. Cihan Ozer <ozer.mc@gmail.com>
2017-10-10 10:08:35 +0300

    Create Observer.hpp

---
+++ ObserverPattern/Observer.hpp
@@ -0,0 +1,23 @@
+#ifndef Observer_hpp
+#define Observer_hpp
+
+#include "OHB_Event.hpp"
+
+class Subject;
+
+// Base class for Observers
+class Observer
+{
+public:
+    
+    virtual void init(Subject* pSubject, const int pId);
+    virtual void handleNotification(OHB_Event pEvent) = 0;
+    
+    int mId;
+    
+protected:
+    
+    Subject* mSubject;
+};
+
+#endif /* Observer_hpp */
