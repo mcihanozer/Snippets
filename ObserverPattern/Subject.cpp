da6c29c6875bb81ad4746593dc27c6b5586d87e9
M. Cihan Ozer <ozer.mc@gmail.com>
2017-10-10 10:11:16 +0300

    Create Subject.cpp

---
+++ ObserverPattern/Subject.cpp
@@ -0,0 +1,47 @@
+// MCO
+
+#include "Subject.hpp"
+
+Subject::Subject()
+{
+    mId = 0;
+}
+
+Subject:: ~Subject()
+{
+    if(mObserverList.size() > 0)
+    {
+        for(int oi = 0; oi < mObserverList.size(); oi++)
+        {
+            mObserverList.erase( mObserverList.begin() + oi );
+        }
+    }
+    
+    mObserverList.clear();
+}
+
+void Subject::attach(Observer *pObserver)
+{
+    mObserverList.push_back(pObserver);
+    pObserver->init(this, mId++);
+}
+
+void Subject::detach(Observer* pObs)
+{
+    for(std::vector<Observer*>::iterator it = mObserverList.begin(); it != mObserverList.end(); it++)
+    {
+        if(pObs->mId == (*it)->mId)
+        {
+            mObserverList.erase(it);
+            break;
+        }
+    }
+}
+
+void Subject::notify(OHB_Event pEvent)
+{
+    for(int oi = 0; oi < mObserverList.size(); oi++)
+    {
+        mObserverList[oi]->handleNotification(pEvent);
+    }
+}
