67c663a5cdbd6e84eefe36f34daf43325cdcbec2
M. Cihan Ozer <ozer.mc@gmail.com>
2018-07-10 11:04:47 +0300

    Create HelperFunctions.hpp

---
+++ FloatComparison/CPP/HelperFunctions.hpp
@@ -0,0 +1,10 @@
+// M. Cihan Ozer - November 2017
+
+// Knuth D.E. The art of computer programming, vol II, section 4.2,
+// especially Floating-Point Comparison 4.2.2, pages 198-220.
+//
+// Also: Boost library Floating-point Comparison page
+extern bool approximatelyEqual(float a, float b);
+extern bool essentiallyEqual(float a, float b);
+extern bool definitelyGreaterThan(float a, float b);
+extern bool definitelyLessThan(float a, float b);
