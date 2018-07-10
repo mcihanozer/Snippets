65f7d019f52894bd832caae00c11f2119577546d
M. Cihan Ozer <ozer.mc@gmail.com>
2018-07-10 11:02:50 +0300

    Update MathHelpers.cs

--- FloatComparison/CS/MathHelpers.cs
+++ FloatComparison/CS/MathHelpers.cs
@@ -1,5 +1,7 @@
 // M. Cihan Ozer - June 2018
 
+using UnityEngine; // A bit of Unity dependency
+
 namespace FloatComparisonHelper
 {
 	public class FloatComparison
@@ -10,23 +12,23 @@ namespace FloatComparisonHelper
 		// Also: Boost library Floating-point Comparison page
 
 		public static bool approximatelyEqual(float a, float b)
-        {         
+    	{
 			return Mathf.Abs(a - b) <= ((Mathf.Abs(a) < Mathf.Abs(b) ? Mathf.Abs(b) : Mathf.Abs(a)) * Mathf.Epsilon);
-        }
+    	}
 
 		public static bool essentiallyEqual(float a, float b)
-        {
+    	{
 			return Mathf.Abs(a - b) <= ((Mathf.Abs(a) > Mathf.Abs(b) ? Mathf.Abs(b) : Mathf.Abs(a)) * Mathf.Epsilon);
-        }
+    	}
 
 		public static bool definitelyGreaterThan(float a, float b)
-        {
+    	{
 			return (a - b) > ((Mathf.Abs(a) < Mathf.Abs(b) ? Mathf.Abs(b) : Mathf.Abs(a)) * Mathf.Epsilon);
-        }
+    	}
 
 		public static bool definitelyLessThan(float a, float b)
-        {
+    	{
 			return (b - a) > ((Mathf.Abs(a) < Mathf.Abs(b) ? Mathf.Abs(b) : Mathf.Abs(a)) * Mathf.Epsilon);
-        }
+    	}
 	}
 }
